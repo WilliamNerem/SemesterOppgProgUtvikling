101ac164d9fdfaf7b468b1c56519c455920264c5
WilliamNerem <william-kn@hotmail.com>
2020-04-06 15:01:03 +0200

    sånn

--- src/main/java/org/openjfx/UserIndexController.java
+++ src/main/java/org/openjfx/UserIndexController.java
@@ -4,7 +4,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -12,21 +11,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
-<<<<<<< HEAD
-public class UserIndexController implements Initializable {
-
-    Component test1 = new Component("ingen","Ingen",1999);
-    Component test2 = new Component("Mus","Wrtt",1999);
-    Component test3 = new Component("Skjermkort", "Yeeeey", 1479);
-    Component test4 = new Component("Harddisk", "jhsfgdjhf", 1479);
-    ObservableList<Component> yeetskrt = FXCollections.observableArrayList(test1, test2, test3, test4);
-    ObservableList<Component> skjermKort123 = FXCollections.observableArrayList();
-    ObservableList<Component> harddisk123 = FXCollections.observableArrayList();
-    ObservableList<Component> mus123 = FXCollections.observableArrayList();
-=======
 public class UserIndexController {
     ObservableList<Component> skjermkortChoose = FXCollections.observableArrayList();
     ObservableList<Component> harddiskChoose = FXCollections.observableArrayList();
@@ -36,7 +21,6 @@ public class UserIndexController {
     ObservableList<Component> motherboardChoose = FXCollections.observableArrayList();
     ObservableList<Component> monitorChoose = FXCollections.observableArrayList();
     ObservableList<Component> componentsBought = FXCollections.observableArrayList();
->>>>>>> master
 
     @FXML
     public ComboBox<Component> cmbSkjermkort;
@@ -111,27 +95,6 @@ public class UserIndexController {
     private Label lblWrongNumber;
 
     @FXML
-<<<<<<< HEAD
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        actualRegister.addComponent(oneComponent);
-        actualRegister.addComponent(twoComponent);
-        ObservableList<Component> components = FXCollections.observableArrayList();
-        components.add(oneComponent);
-        components.add(twoComponent);
-        cmbSkjermkort.valueProperty();
-        componentcmb.setItems(components);
-        cmbSkjermkort.setItems();
-    }
-
-    ComboBox<Component> componentcmb = new ComboBox<>();
-    ComponentRegister actualRegister = new ComponentRegister();
-    Component oneComponent = new Component("Skjermkort","ASUS ROG",1900);
-    Component twoComponent = new Component("Skjermkort","GEFORCE GTX 1080", 3500);
-
-    @FXML
-    void chooseHarddisk(ActionEvent event) {
-
-=======
     void chooseHarddisk(KeyEvent event) {
         CheckArrayOfComponents checkArray = new CheckArrayOfComponents();
         for(Component c : checkArray.checkComponentAll) {
@@ -139,7 +102,6 @@ public class UserIndexController {
                 lblHarddisk.setText((CalculatePrice.calcComponent(c.getPrize(),Integer.parseInt(txtAntallHarddisk.getText())))+" kr");
             }
         }
->>>>>>> master
     }
 
     @FXML
@@ -186,18 +148,12 @@ public class UserIndexController {
     }
 
     @FXML
-<<<<<<< HEAD
-    void chooseSkjermkort(ActionEvent event) {
-        if(CountException.check(txtAntallSkjermkort.getText()).equals("")) {
-            lblSkjermkort.setText(CalculatePrice.calcComponent(oneComponent.getPrize(), Integer.parseInt(txtAntallSkjermkort.getText())));
-=======
     void chooseSkjermkort(KeyEvent event) {
         CheckArrayOfComponents checkArray = new CheckArrayOfComponents();
         for(Component c : checkArray.checkComponentAll) {
             if(cmbSkjermkort.getValue().toString().equals(c.getName())) {
                 lblSkjermkort.setText((CalculatePrice.calcComponent(c.getPrize(),Integer.parseInt(txtAntallSkjermkort.getText())))+" kr");
             }
->>>>>>> master
         }
     }
 
@@ -227,10 +183,10 @@ public class UserIndexController {
         CheckArrayOfComponents checkArray = new CheckArrayOfComponents();
         if(lblWrongNumber.getText().equals("")) {
             for(Component c : checkArray.checkComponentAll) { {
-                    if (c.getName().equals(cmbHarddisk.getValue().toString()) || c.getName().equals((cmbSkjermkort.getValue().toString())) || c.getName().equals(cmbMus.getValue().toString())) {
-                        componentsBought.add(c);
-                    }
+                if (c.getName().equals(cmbHarddisk.getValue().toString()) || c.getName().equals((cmbSkjermkort.getValue().toString())) || c.getName().equals(cmbMus.getValue().toString())) {
+                    componentsBought.add(c);
                 }
+            }
             }
         }
         System.out.print(componentsBought);
