48b11575e6bf66b940c7ff00e458a1405e440e26
WilliamNerem <william-kn@hotmail.com>
2020-04-06 14:56:31 +0200

    hey

--- src/main/java/org/openjfx/InvalidComponentFormatException.java
+++ src/main/java/org/openjfx/InvalidComponentFormatException.java
@@ -2,7 +2,7 @@ package org.openjfx;
 
 import java.io.IOException;
 
-public class InvalidComponentFormatException extends IOException {
+public class InvalidComponentFormatException extends IOException { 
     public InvalidComponentFormatException(String msg) {
         super(msg);
     }
