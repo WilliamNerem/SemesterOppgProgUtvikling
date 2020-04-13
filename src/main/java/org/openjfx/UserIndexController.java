a62b0aab4e4161e8d802419835a7af1f48835b37
Martin <brandalmartin@gmail.com>
2020-04-13 12:32:35 +0200

    fixa method i initalize

--- src/main/java/org/openjfx/UserIndexController.java
+++ src/main/java/org/openjfx/UserIndexController.java
@@ -4,9 +4,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import org.openjfx.*;
 import org.openjfx.Feilhåndtering.InputException;
 
@@ -182,7 +187,7 @@ public class UserIndexController {
     void chooseTastatur(ActionEvent event) {
         String output = "";
         try {
-            lblTastatur.setText(InputException.checkInput(cmbTastatur, txtAntallTastatur.getValue()));
+            output = InputException.checkInput(cmbTastatur, txtAntallSkjermkort.getValue());
             lblWrong.setText("");
         } catch (InputException.InvalidInputException e) {
             lblWrong.setText(e.getMessage());
@@ -247,7 +252,14 @@ public class UserIndexController {
                     ComponentAndAntall compBoughtMinne = new ComponentAndAntall(c.getType(), c.getName(), txtAntallMinne.getValue(), c.getPrice());
                     componentsBought.add(compBoughtMinne);
                 }
-                App.setRoot("handlekurv");
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("org.openjfx/handlekurv.fxml"));
+
+                HandlekurvController hkController = loader.getController();
+               // hkController.setTable(componentsBought);
+                // hkController.handlekurvArray = componentsBought;
+                Stage stage = new Stage();
+                stage.setScene(new Scene ( (Pane) loader.load()));
+                stage.show();
             }
         }
         System.out.print(componentsBought);
@@ -257,34 +269,7 @@ public class UserIndexController {
         CheckArrayOfComponents checkArray = new CheckArrayOfComponents();
         HandlekurvController hkc = new HandlekurvController();
         lblAntallInHandlekurv.setText("("+hkc.numberInHandlevogn+")");
-        cmbMus.setItems(checkArray.checkmus());
-        cmbMus.getSelectionModel().selectFirst();
-        cmbSkjermkort.setItems(checkArray.checkSkjermkort());
-        cmbSkjermkort.getSelectionModel().selectFirst();
-        cmbHarddisk.setItems(checkArray.checkHarddisk());
-        cmbHarddisk.getSelectionModel().selectFirst();
-        cmbMotherboard.setItems((checkArray.checkMotherboard()));
-        cmbMotherboard.getSelectionModel().selectFirst();
-        cmbTastatur.setItems((checkArray.checkTastatur()));
-        cmbTastatur.getSelectionModel().selectFirst();
-        cmbMinne.setItems((checkArray.checkMinne()));
-        cmbMinne.getSelectionModel().selectFirst();
-        cmbMonitor.setItems((checkArray.checkMonitor()));
-        cmbMonitor.getSelectionModel().selectFirst();
-        txtAntallSkjermkort.setItems(checkArray.antall);
-        txtAntallSkjermkort.getSelectionModel().selectFirst();
-        txtAntallHarddisk.setItems(checkArray.antall);
-        txtAntallHarddisk.getSelectionModel().selectFirst();
-        txtAntallTastatur.setItems(checkArray.antall);
-        txtAntallTastatur.getSelectionModel().selectFirst();
-        txtAntallMotherboard.setItems(checkArray.antall);
-        txtAntallMotherboard.getSelectionModel().selectFirst();
-        txtAntallMonitor.setItems(checkArray.antall);
-        txtAntallMonitor.getSelectionModel().selectFirst();
-        txtAntallMinne.setItems(checkArray.antall);
-        txtAntallMinne.getSelectionModel().selectFirst();
-        txtAntallMus.setItems(checkArray.antall);
-        txtAntallMus.getSelectionModel().selectFirst();
+        checkArray.setAll(cmbMus,cmbSkjermkort,cmbHarddisk,cmbMotherboard,cmbTastatur,cmbMinne,cmbMonitor,txtAntallSkjermkort,txtAntallHarddisk,txtAntallTastatur,txtAntallMotherboard,txtAntallMonitor,txtAntallMinne,txtAntallMus);
     }
 
 }
